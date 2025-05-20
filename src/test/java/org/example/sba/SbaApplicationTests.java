eaf32c11d259e37771d5cb3e2f7b54e02c971f7b
nhducminhqt <email@gmail.com>
2025-05-21 00:01:30 +0700

    feat(DB): first config to connection database

---
+++ src/test/java/org/example/sba/SbaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
